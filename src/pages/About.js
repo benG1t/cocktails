4ef49ec4e19f7286404bb1abd4a345761695872f
benjamin efosa <benjaminefosa@yahoo.com>
2022-07-26 11:48:07 +0100

    cocktails

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, dolores,
+        voluptatibus libero ab reprehenderit ipsum earum doloremque voluptate
+        iusto sed ut impedit architecto voluptas sit cumque error iure
+        consequuntur possimus dolore illum, minus deleniti quaerat officia? Quam
+        veniam earum a.
+      </p>
+    </section>
+  );
+};
+
+export default About;
